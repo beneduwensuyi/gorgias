a89fb9dd04326f627232d6dcaee7f29fa736f0b8
beneduwensuyi <ben.eduwensuyi@hashicorp.com>
2020-10-12 14:51:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,9 @@ module "make_a_vm" {
   source  = "app.terraform.io/beduwensuyi/make-a-vm/aws"
   version = "1.0.1"
 
-  tag_name = "newVm"
+  os = "ubuntu"
+  size = "t2.small"
+  tag_name = "servername"
   tag_owner = "beduwensuyi"
-  tag_ttl = "24h"
+  tag_ttl = 72
 }
