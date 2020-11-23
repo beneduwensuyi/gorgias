d36aae12a96067ccc0ac6a9969ef2b227bbece43
Benjamin Eduwensuyi <b.eduwensuyi@Bens-MBP.attlocal.net>
2020-11-23 16:14:33 -0600

    main files

--- main.tf
+++ main.tf
@@ -13,25 +13,3 @@ resource "aws_instance" "east_example" {
   ami           = var.amis[var.region]
   instance_type = "t2.xlarge"    # "t2.micro"
 }
-
-
-# terraform {
-#   required_providers {
-#     splunk = {
-#       source = "splunk/splunk"
-#       version = "1.3.1"
-#     }
-#   }
-# }
-
-# provider "splunk" {
-#   url                  = "splunk01.corp.local:8089"
-#   username             = "beduwensuyi"
-#   password             = var.password
-# }
-
-# resource "splunk_apps_local" "terraform_splunk_app" {
-#   filename         = true
-#   name             = "/tmp/terraform-cloud-for-splunk_005.tgz"
-#   explicit_appname = "terraform_cloud_for_splunk"
-# }
